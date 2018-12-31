634304e468e1adc09f3e178843ce7ac905d66d6f
Hyeon-Ki Seong <hynkis@gmail.com>
2018-12-31 22:21:29 +0900

    Update 1.2_maxTwoDiff.py

--- 1.2_maxTwoDiff.py
+++ 1.2_maxTwoDiff.py
@@ -1,1 +1,18 @@
+def maxTwoDiff1(nums):
+    """
+    nums.sort() : O(NlogN)
+    """
+    nums.sort()
+    return nums[-1] - nums[0]
 
+def maxTwoDiff(nums):
+    """
+    max, min : O(N)
+    """
+    return max(nums) - min(nums)
+
+def main():
+    print(maxTwoDiff([2, 8, 19, 37, 4, 5, 12, 50, 1, 34, 23])) # should return 49
+    
+if __name__ == "__main__":
+    main()
