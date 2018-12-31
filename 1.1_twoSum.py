97aa28f4fb1ca700faa9408a01734a8abde230b0
Hyeon-Ki Seong <hynkis@gmail.com>
2018-12-31 21:24:32 +0900

    Create 1.1_twoSum.py

---
+++ 1.1_twoSum.py
@@ -0,0 +1,10 @@
+def twoSum1(nums, target):
+    for num1 in nums:
+      if target == num1 + num2:
+          return num1, num2
+          
+def main():
+    print(twoSum([2, 8, 19, 37, 4, 5], 12)
+    
+if __name__ == "__main__":
+    main()
