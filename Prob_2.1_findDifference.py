72f19fa6207ff517ff8b050d8d133c7f620554aa
Hyeon-Ki Seong <hynkis@gmail.com>
2019-01-13 18:35:21 +0900

    Create Prob_2.1_findDifference.py

---
+++ Prob_2.1_findDifference.py
@@ -0,0 +1,33 @@
+"""
+틀린 문자 찾기
+두 개의 문자열이 주어집니다. 이때 두번째 문자열은 첫번째 문자열에 하나의 문자를 추가 한 후, 그 순서를 랜덤하게 뒤섞은 문자입니다. 이때 추가된 문자를 찾아 보도록 합시다.
+
+예를 들어서, apple 과 azlppe 가 주어졌을 경우 추가된 문자는 z입니다.
+
+추가된 문자는 하나라고 가정해도 좋습니다.
+추가된 문자가 이미 리스트에 존재하던 문자 일 수도 있습니다.
+"""
+
+def findDifference(str1, str2):
+    M1 = {}
+    M2 = {}
+    
+    for char in str1:
+        M1[char] = M1.get(char, 0) + 1
+        
+    for char in str2:
+        if char not in M1:
+            return char
+        M2[char] = M2.get(char, 0) + 1
+        
+    for key in M2.keys():
+        if M2[key] != M1[key]:
+            return key
+
+def main():
+    print(findDifference("apple", "azlppe"))
+    print(findDifference("asdfzxcv", "fdsaxxczv"))
+    
+
+if __name__ == "__main__":
+    main()
